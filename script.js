e2ce58a1a471b4901733bf2db28298ef87f783b3
Oliwier <wiecko.oli@gmail.com>
2024-08-11 23:29:05 +0100

    Button edit

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const player = document.getElementById('player');
 
 let playing = false;
 
+player.addEventListener('ended', handleTrackEnd);
+
+function handleTrackEnd() {
+    playing = false;
+}
+
 function handleClick() {
     if (playing) {
         player.pause();
+        player.currentTime = 0;
         playing = false;
     } else {
         player.play();
